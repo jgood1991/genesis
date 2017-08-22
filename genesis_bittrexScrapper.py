25b268207fb1ab788028709b5d8b5bbbde2ad105
santaclause88 <anirudh.kashyap@gmail.com>
2017-08-21 21:19:17 -0500

    Update genesis_bittrexScrapper.py
    Cleaned up comments

--- genesis_bittrexScrapper.py
+++ genesis_bittrexScrapper.py
@@ -18,22 +18,16 @@ url = 'https://bittrex.com/api/v1.1/public/getmarketsummaries'
 counter = 1
 while True:
 	response = requests.get(url)
-	#data = response.json()
 	json_data = json.loads(response.text)
 	responseList = json_data["result"]
 	howLong =  len(responseList)
-	#print howLong
 	counterI = 0
 	while counterI < howLong - 1 :
         	counterI = counterI + 1
-		#doc_id = collections.insert_one(responseList[counterI]).inserted_id
-		#db.collections.update({noExist: true}, {"$setOnInsert": responseList[counterI]}, upsert=True)
 		collections.update(responseList[counterI], responseList[counterI], upsert=True)
-		#cursor = collections.find(responseList[counterI]).upsert().update_one({"$setOnInsert": responseList[counterI],"$set": {}})
-		#print doc_id
 		counter = counter + 1
 		print counter
 
-#	print "TEST IS IT HERE"
+
 	print
-#	sleep(.05)
+
