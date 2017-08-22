b62359218697b59490713c62886864c6d95813ca
santaclause88 <anirudh.kashyap@gmail.com>
2017-08-21 21:23:54 -0500

    Update genesis_logic.py

--- genesis_logic.py
+++ genesis_logic.py
@@ -12,19 +12,13 @@ def printLastNumbers( market , number):
 	myGraphList = []
 	cursor = collections.find({"MarketName": market }).sort("TimeStamp", -1)
 	for document in cursor:
-		#pprint(document['Ask'])
-		#pprint(document['TimeStamp'])
-		#pprint(document['MarketName'])
 		myGraphList.append([document['Last'],document['TimeStamp'],document['MarketName']])
-		#mySetTemp = set(myGraphList)
 		myGraphList = [list(t) for t in set(tuple(element) for element in myGraphList)]
-		#number = number - 1
 		if (number == len(myGraphList)):
 			myGraphList = sorted(myGraphList,key=lambda l:l[1], reverse=True)
 			return myGraphList
 	return myGraphList
-		#print '-----------------------------------------------------------------------------------------------------------------------------------------------'
-
+		
 #Function that will return a list of the different markets
 def getMarkets():
 	url = 'https://bittrex.com/api/v1.1/public/getmarketsummaries'
@@ -37,10 +31,9 @@ def getMarkets():
 	counterI = 0
 	while counterI < howLong - 1 :
 		counterI = counterI + 1
-		#pprint(responseList[counterI]['MarketName'])
 		if responseList[counterI]["BaseVolume"] > 1000:
 			myMarketList.append(responseList[counterI]['MarketName'])
-		#print "______________________________________________________________________________________________________________________________________________________"
+		
 	return myMarketList
 
 
@@ -50,7 +43,6 @@ def parseTimeStamps(myList):
 		format = '%Y-%m-%d %H:%M:%S'
 		x[1] = str(x[1]).replace("T", " ")
 		x[1] = str(x[1]).split(".", 1)[0]
-		#print x[2] + "---------------" + x[1] + "-----------" + str(datetime.strptime(x[1], format))
 		x[1] = datetime.strptime(x[1], format)
 
 	return myList
@@ -61,12 +53,12 @@ def normalizeTimeStamps(myList):
 	howLong = len(myList)
 	counterI = 0
 	while counterI < howLong  :
-		#myList[counterI][1] = str(myList[0][1] - myList[counterI][1])
-		#print myList[0][1] - myList[counterI][1]
 		normalizedList.append([myList[counterI][0], (myList[0][1] - myList[counterI][1]).total_seconds(), myList[0][2]])
 		counterI = counterI + 1
 
 	return normalizedList
+
+
 #Establishing connection to MongoDB database
 client = MongoClient(host=['192.168.1.198:27017'])
 #client = MongoClient(host=['108.196.157.14:27017'])
